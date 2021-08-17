b19d237b302438a2a8def041b58b88bae302e5f4
Anikait Singh <singh.anikait@gmail.com>
2021-08-16 20:39:42 -0700

    updated algo

--- rlkit/torch/sac/td3bc.py
+++ rlkit/torch/sac/td3bc.py
@@ -1,8 +1,5 @@
 from collections import OrderedDict
-
-import numpy as np
 import torch
-import torch.optim as optim
 from torch import nn as nn
 
 import rlkit.torch.pytorch_util as ptu
@@ -10,18 +7,15 @@ from rlkit.core.eval_util import create_stats_ordered_dict
 from rlkit.torch.torch_rl_algorithm import TorchTrainer
 
 
-class BRACTrainer(TorchTrainer):
+class TD3BCTrainer(TorchTrainer):
     def __init__(
             self,
             env,
             policy,
-            behavior_policy,
             qf1,
             qf2,
             target_qf1,
             target_qf2,
-
-            beta = 1.0,
 
             discount=0.99,
             reward_scale=1.0,
@@ -29,38 +23,23 @@ class BRACTrainer(TorchTrainer):
             policy_lr=1e-3,
             qf_lr=1e-3,
             optimizer_class=optim.Adam,
+            beta = 1.0, 
 
             soft_target_tau=1e-2,
             target_update_period=1,
             plotter=None,
             render_eval_paths=False,
-
-            use_automatic_entropy_tuning=True,
-            target_entropy=None,
     ):
         super().__init__()
         self.env = env
         self.policy = policy
-        self.behavior_policy = behavior_policy
-        self.beta = beta
         self.qf1 = qf1
         self.qf2 = qf2
         self.target_qf1 = target_qf1
         self.target_qf2 = target_qf2
         self.soft_target_tau = soft_target_tau
         self.target_update_period = target_update_period
-
-        self.use_automatic_entropy_tuning = use_automatic_entropy_tuning
-        if self.use_automatic_entropy_tuning:
-            if target_entropy:
-                self.target_entropy = target_entropy
-            else:
-                self.target_entropy = -np.prod(self.env.action_space.shape).item()  # heuristic value from Tuomas
-            self.log_alpha = ptu.zeros(1, requires_grad=True)
-            self.alpha_optimizer = optimizer_class(
-                [self.log_alpha],
-                lr=policy_lr,
-            )
+        self.beta = beta
 
         self.plotter = plotter
         self.render_eval_paths = render_eval_paths
@@ -100,50 +79,25 @@ class BRACTrainer(TorchTrainer):
         """
         Policy and Alpha Loss
         """
-        new_obs_actions, policy_mean, policy_log_std, log_pi, *_ = self.policy(
-            obs, reparameterize=True, return_log_prob=True,
-        )
-
-        log_pi_behavior = self.behavior_policy.log_prob(obs,new_obs_actions)
-
-        if self.use_automatic_entropy_tuning:
-            alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
-            self.alpha_optimizer.zero_grad()
-            alpha_loss.backward()
-            self.alpha_optimizer.step()
-            alpha = self.log_alpha.exp()
-        else:
-            alpha_loss = 0
-            alpha = 1
-
+        new_obs_actions = self.policy(obs)
         q_new_actions = torch.min(
             self.qf1(obs, new_obs_actions),
             self.qf2(obs, new_obs_actions),
         )
-        policy_loss = (alpha*log_pi - self.beta * log_pi_behavior - q_new_actions).mean()
+        policy_loss = ((actions - q_new_actions)**2 + self.beta * q_new_actions).mean()
 
         """
         QF Loss
         """
         q1_pred = self.qf1(obs, actions)
         q2_pred = self.qf2(obs, actions)
-        # Make sure policy accounts for squashing functions like tanh correctly!
-        new_next_actions, _, _, new_log_pi, *_ = self.policy(
-            next_obs, reparameterize=True, return_log_prob=True,
-        )
 
-        new_log_pi_behavior= self.behavior_policy.log_prob(next_obs, new_next_actions)
-
-
-        target_q_values = torch.min(
-            self.target_qf1(next_obs, new_next_actions),
-            self.target_qf2(next_obs, new_next_actions),
-        ) - alpha * new_log_pi + self.beta * new_log_pi_behavior
+        new_next_actions = self.policy(next_obs)
+        target_q_values = torch.min(self.target_qf1(next_obs, new_next_actions), self.target_qf2(next_obs, new_next_actions))
 
         q_target = self.reward_scale * rewards + (1. - terminals) * self.discount * target_q_values
         qf1_loss = self.qf_criterion(q1_pred, q_target.detach())
         qf2_loss = self.qf_criterion(q2_pred, q_target.detach())
-
 
         """
         Update networks
@@ -180,7 +134,7 @@ class BRACTrainer(TorchTrainer):
             Eval should set this to None.
             This way, these statistics are only computed for one batch.
             """
-            policy_loss = (log_pi - q_new_actions).mean()
+            policy_loss = ((actions - q_new_actions)**2 + self.beta * q_new_actions).mean()
 
             self.eval_statistics['QF1 Loss'] = np.mean(ptu.get_numpy(qf1_loss))
             self.eval_statistics['QF2 Loss'] = np.mean(ptu.get_numpy(qf2_loss))
@@ -199,21 +153,6 @@ class BRACTrainer(TorchTrainer):
                 'Q Targets',
                 ptu.get_numpy(q_target),
             ))
-            self.eval_statistics.update(create_stats_ordered_dict(
-                'Log Pis',
-                ptu.get_numpy(log_pi),
-            ))
-            self.eval_statistics.update(create_stats_ordered_dict(
-                'Policy mu',
-                ptu.get_numpy(policy_mean),
-            ))
-            self.eval_statistics.update(create_stats_ordered_dict(
-                'Policy log std',
-                ptu.get_numpy(policy_log_std),
-            ))
-            if self.use_automatic_entropy_tuning:
-                self.eval_statistics['Alpha'] = alpha.item()
-                self.eval_statistics['Alpha Loss'] = alpha_loss.item()
         self._n_train_steps_total += 1
 
     def get_diagnostics(self):
